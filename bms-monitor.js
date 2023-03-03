a1f710de80a35327aacf817507be870e6a4f80e0
Anusha32 <anushabr3@gmai.com>
2023-03-03 15:27:33 +0530

    update

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,18 +1,12 @@
 const {expect} = require('chai');
 const {rangeCheck} = require('./rangeCheck');
+const {statusindicator} = require('./statusIndicator')
 
 function batteryIsOk(temperature, soc, charge_rate) {
-    let isbatteryOk = true;
-    
+
     temp = rangeCheck(temperature, 0, 45);
     stateOfCharge = rangeCheck(soc, 20, 80);
     chargeRate = rangeCheck(charge_rate,0,0.8);
-
-    if(temp == false || stateOfCharge == false || chargeRate == false){
-        isbatteryOk = false;
-    }
-
-   return isbatteryOk;
 }
 
 module.exports = {batteryIsOk};
