36648d15b83dd58893e6a131a853313163b2933a
Anusha32 <anushabr3@gmai.com>
2023-03-03 15:34:23 +0530

    test cases updated

--- test/test.js
+++ test/test.js
@@ -6,21 +6,28 @@ describe("Test case to check battery", function(){
         assert.equal(batteryIsOk(25,50,0.5),true,"battery is okay");
     });
     it('Battery temperature is high', function () {
-        assert.equal(batteryIsOk(47,60,0.6),true,"battery temperature is not okay") 
+        assert.equal(batteryIsOk(47,60,0.6),false,"battery temperature is not okay") 
     });
     it('Battery temperature is low', function () {
-        assert.equal(batteryIsOk(-1,60,0.6),true,"battery temperature is not okay") 
+        assert.equal(batteryIsOk(-1,60,0.6),false,"battery temperature is not okay") 
     });
     it('State of charge is low', function () {
-        assert.equal(batteryIsOk(25,10,0.6),true,"battery state is not okay") 
+        assert.equal(batteryIsOk(25,10,0.6),false,"battery state is not okay") 
     });
     it('State of charge is high', function () {
-        assert.equal(batteryIsOk(25,85,0.6),true,"battery state is not okay") 
+        assert.equal(batteryIsOk(25,85,0.6),false,"battery state is not okay") 
     });
     it('Charge rate is low', function () {
-        assert.equal(batteryIsOk(25,60,-0.1),true,"battery charge rate is not okay") 
+        assert.equal(batteryIsOk(25,60,-0.1),false,"battery charge rate is not okay") 
     });
     it('Charge rate is high', function () {
-        assert.equal(batteryIsOk(25,85,0.9),true,"battery is not okay") 
+        assert.equal(batteryIsOk(25,85,0.9),false,"battery is not okay") 
+    });
+    it('All parameters at minimum value', function() {
+        assert.equal(batteryIsOk(0, 20, 0), true);
+    });
+      
+    it('All parameters at maximum value', function() {
+        assert.equal(batteryIsOk(45, 80, 0.8), true);
     });
 });
