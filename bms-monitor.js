240e7affaab20db5af58c0e354c3f58903521c1b
Anusha32 <anushabr3@gmai.com>
2023-03-05 14:39:32 +0530

    updated to arrow function

--- bms-monitor.js
+++ bms-monitor.js
@@ -8,16 +8,14 @@ function batteryIsOk(temperature, soc, charge_rate,tolerance) {
         {name:"stateOfCharge", input: soc, min:20, max:80},
         {name:"chargeRate", input:charge_rate, min:0, max:0.8}
     ];
-    let isbatteryOk = values.every(rangeCheck);
-
-    function rangeCheck(item){
-       if (item.input < item.min || item.input > item.max){
-        return false;
-       }
-       else{
-        return true;
-       }
-    }
+    let isbatteryOk = values.every((item)=>{
+        if (item.input < item.min || item.input > item.max){
+         return false;
+        }
+        else{
+         return true;
+        }
+     })
 
 
 
