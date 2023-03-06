2be693b046ad181f33490b2259ddc52ac2090ef0
Anusha32 <anushabr3@gmai.com>
2023-03-06 15:47:19 +0530

    temperature unit update

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,11 +1,15 @@
 const {expect} = require('chai');
+const language = require('./changeLanguage');
+const { celciusTemp } = require('./celciusTemp');
 
-function batteryIsOk(temperature, soc, charge_rate,tolerance) {
+
+function batteryIsOk(temperature, soc, charge_rate,tempUnit) {
 
 
+    temperature = celciusTemp(temperature,tempUnit);
     const values = [
-        {name:"temperature", input: temperature, min:0, max:45},
-        {name:"stateOfCharge", input: soc, min:20, max:80},
+        {name:"temperature", input:temperature, min:0, max:45},
+        {name:"stateOfCharge", input:soc, min:20, max:80},
         {name:"chargeRate", input:charge_rate, min:0, max:0.8}
     ];
     let isbatteryOk = values.every((item)=>{
