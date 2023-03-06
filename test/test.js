cbdfb40c0b75085d9a0ea053226c218ab1492007
Anusha32 <anushabr3@gmai.com>
2023-03-06 13:12:56 +0530

    test updated

--- test/test.js
+++ test/test.js
@@ -3,31 +3,31 @@ const {batteryIsOk} = require('../bms-monitor');
 
 describe("Test case to check battery", function(){
     it("Battery is good", function(){
-        assert.equal(batteryIsOk(25,"Celcius",50,0.5),true,"battery is okay");
+        assert.equal(batteryIsOk(25,50,0.5),true,"battery is okay");
     });
     it('Battery temperature is high', function () {
-        assert.equal(batteryIsOk(47,"Celcius",60,0.6),false,"battery temperature is not okay") 
+        assert.equal(batteryIsOk(47,60,0.6),false,"battery temperature is not okay") 
     });
     it('Battery temperature is low', function () {
-        assert.equal(batteryIsOk(-1,"Fahrenheit",60,0.6),false,"battery temperature is not okay") 
+        assert.equal(batteryIsOk(-1,60,0.6),false,"battery temperature is not okay") 
     });
     it('State of charge is low', function () {
-        assert.equal(batteryIsOk(25,"Celcius",10,0.6),false,"battery state is not okay") 
+        assert.equal(batteryIsOk(25,10,0.6),false,"battery state is not okay") 
     });
     it('State of charge is high', function () {
-        assert.equal(batteryIsOk(25,"Fahrenheit",85,0.6),false,"battery state is not okay") 
+        assert.equal(batteryIsOk(25,85,0.6),false,"battery state is not okay") 
     });
     it('Charge rate is low', function () {
-        assert.equal(batteryIsOk(25,"Fahrenheit",60,-0.1),false,"battery charge rate is not okay") 
+        assert.equal(batteryIsOk(25,60,-0.1),false,"battery charge rate is not okay") 
     });
     it('Charge rate is high', function () {
-        assert.equal(batteryIsOk(25,"Fahrenheit",85,0.9),false,"battery is not okay") 
+        assert.equal(batteryIsOk(25,85,0.9),false,"battery is not okay") 
     });
     it('All parameters at minimum value', function() {
-        assert.equal(batteryIsOk(0,"Celcius", 20, 0), true);
+        assert.equal(batteryIsOk(0, 20, 0), true);
     });
       
     it('All parameters at maximum value', function() {
-        assert.equal(batteryIsOk(45,"Fahrenheit", 80, 0.8), true);
+        assert.equal(batteryIsOk(45, 80, 0.8), true);
     });
 });
