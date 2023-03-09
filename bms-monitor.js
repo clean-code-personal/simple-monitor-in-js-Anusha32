c840f923355b93b908cb672c723c2fccf694cc7a
Anusha32 <anushabr3@gmai.com>
2023-03-09 15:51:58 +0530

    langugage function added

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,29 +1,29 @@
-const {expect} = require('chai');
-const language = require('./changeLanguage');
-const { celciusTemp } = require('./celciusTemp');
+const { expect } = require("chai");
+const lang = require("./changeLanguage");
+const { celciusTemp } = require("./celciusTemp");
 
+function batteryIsOk(temperature, soc, charge_rate, tempUnit) {
+  temperature = celciusTemp(temperature, tempUnit);
+  const values = [
+    { name: "temperature", input: temperature, min: 0, max: 45 },
+    { name: "stateOfCharge", input: soc, min: 20, max: 80 },
+    { name: "chargeRate", input: charge_rate, min: 0, max: 0.8 },
+  ];
 
-function batteryIsOk(temperature, soc, charge_rate,tempUnit) {
+  temperature = lang.temperature;
+  soc = lang.soc;
+  chargeRate = lang.chargeRate;
 
+  console.log(`${temperature}, ${soc}, ${chargeRate}`);
+  let isbatteryOk = values.every((item) => {
+    if (item.input < item.min || item.input > item.max) {
+      return false;
+    } else {
+      return true;
+    }
+  });
 
-    temperature = celciusTemp(temperature,tempUnit);
-    const values = [
-        {name:"temperature", input:temperature, min:0, max:45},
-        {name:"stateOfCharge", input:soc, min:20, max:80},
-        {name:"chargeRate", input:charge_rate, min:0, max:0.8}
-    ];
-    let isbatteryOk = values.every((item)=>{
-        if (item.input < item.min || item.input > item.max){
-         return false;
-        }
-        else{
-         return true;
-        }
-     })
-
-
-
-   return isbatteryOk;
+  return isbatteryOk;
 }
 
-module.exports = {batteryIsOk};
+module.exports = { batteryIsOk };
